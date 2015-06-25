80a616183d27ffcd7c19ddbd45985e22bd9b3d34
nadia kartikasari <kartikasnadia@gmail.com>
2015-06-25 20:19:02 +0700

    translate

--- app/src/main/java/clientsocket/example/com/droidslator/MainActivity.java
+++ app/src/main/java/clientsocket/example/com/droidslator/MainActivity.java
@@ -58,3 +58,73 @@ import android.widget.TextView;
 import com.memetix.mst.language.Language;
 import com.memetix.mst.translate.Translate;
 
+//import clientsocket.example.com.droidslator.language.Language;
+
+public class MainActivity extends Activity implements OnInitListener {
+
+    private TextToSpeech tts;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*tts*/
+
+        ((Button) findViewById(R.id.bTranslate)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+
+
+                class bgStuff extends AsyncTask<Void, Void, Void>{
+
+                    String translatedText = "";
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // TODO Auto-generated method stub
+                        try {
+                            String text = ((EditText) findViewById(R.id.etUserText)).getText().toString();
+                            translatedText = translate(text);
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                            translatedText = e.toString();
+                        }
+
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        // TODO Auto-generated method stub
+                        ((TextView) findViewById(R.id.tvTranslatedText)).setText(translatedText);
+                        super.onPostExecute(result);
+                    }
+
+                }
+
+                new bgStuff().execute();
+            }
+        });
+    }
+
+    public String translate(String text) throws Exception{
+
+
+        // Set the Client ID / Client Secret once per JVM. It is set statically and applies to all services
+        Translate.setClientId("nadiacino"); //Change this
+        Translate.setClientSecret("mARVAGmsIC/LBCsnrNJrIkuM8YGp7v3tiTfKjXLEbLk=");
+
+
+        String translatedText = "";
+
+        translatedText = Translate.execute(text,Language.FRENCH);
+
+        return translatedText;
+    }
+
+
+
+}
